f251d42445b1f365f87f046adb48f63fb459889b
Frodonche <guiguix3@hotmail.fr>
2017-11-23 21:56:09 +0100

    Premier commit

---
+++ test.pl
@@ -0,0 +1,26 @@
+% CODE FOURNI PAR LE PROF TAVU
+
+% Prédicats d'affichage fournis
+
+% set_echo: ce prédicat active l'affichage par le prédicat echo
+set_echo :- assert(echo_on).
+
+% clr_echo: ce prédicat inhibe l'affichage par le prédicat echo
+clr_echo :- retractall(echo_on).
+
+% echo(T): si le flag echo_on est positionné, echo(T) affiche le terme T
+%          sinon, echo(T) réussit simplement en ne faisant rien.
+
+echo(T) :- echo_on, !, write(T).
+echo(_).
+
+
+% CODE TROUVE POUR TEST
+
+food(burger).
+food(sandwich).
+food(pizza).
+lunch(sandwich).
+dinner(pizza).
+
+meal(X) :- food(X).
